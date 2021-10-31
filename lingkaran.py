07d1aef40151b54a34c1fb46e039241c5d8cd9eb
rezza fauzi <rezzaf205@gmail.com>
2021-10-31 17:28:42 +0700

    menambahkan lingkaran.py

--- lingkaran.py
+++ lingkaran.py
@@ -4,5 +4,5 @@ r = float(input("Masukan Jari-jari : "))
 luas = math.pi*(r*r)
 keliling = 2*math.pi*r
 
-print ("Luas Lingkaran \t\t= ",luas)
-print ("Keliling Lingkaran\t= ",keliling)
+print ("Luas Lingkaran \t= ",format(luas,'.2f'))
+print ("Keliling Lingkaran \t= ",format(keliling,'.2f'))
