137bb14ff64622cb925f4c8590519da6eed86b16
rezza fauzi <rezzaf205@gmail.com>
2021-10-31 16:24:11 +0700

    menambahkan file lingkaran.py

---
+++ lingkaran.py
@@ -0,0 +1,8 @@
+import math
+r = float(input("Masukan Jari-jari : "))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print ("Luas Lingkaran \t\t= ",luas)
+print ("Keliling Lingkaran\t= ",keliling)
